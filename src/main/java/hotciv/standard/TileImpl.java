ba3664d2e25b0c0d8ac109cbba202e7d02798a04
Cody <cody.cacal@yahoo.com>
2022-09-14 13:38:16 -0400

    Created unitImpl

--- src/main/java/hotciv/standard/TileImpl.java
+++ src/main/java/hotciv/standard/TileImpl.java
@@ -5,9 +5,7 @@ import hotciv.framework.Tile;
 public class TileImpl implements Tile {
     private String type;
 
-    public TileImpl(String type) {
-        this.type = type;
-    }
+    public TileImpl(String type) { this.type = type; }
 
     public String getTypeString(){
         return type;
