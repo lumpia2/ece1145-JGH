eb395b5978106568dca0131b8cc53067b8358ef6
lumpia2 <cody.cacal@yahoo.com>
2022-11-10 14:02:41 -0500

    Added prints statements stating the actions of the functions whenever called.

--- src/main/java/hotciv/standard/GameTranscriptDecorator.java
+++ src/main/java/hotciv/standard/GameTranscriptDecorator.java
@@ -5,81 +5,161 @@ import hotciv.framework.*;
 public class GameTranscriptDecorator implements Game {
     private Game game;
 
-    public GameTranscriptDecorator(Game game) {
-        
-    }
-    @Override
-    public Tile getTileAt(Position p) {
-        return null;
-    }
+    public GameTranscriptDecorator(Game game)
+    {
+        this.game = game;
+        Player currPlayer = game.getPlayerInTurn();
 
-    @Override
-    public Unit getUnitAt(Position p) {
-        return null;
+        System.out.println("Game created.");
+        System.out.println(currPlayer.toString() + " player starts first.");
     }
-
     @Override
-    public City getCityAt(Position p) {
-        return null;
-    }
+    public Tile getTileAt(Position p)
+    {
+        Tile currTile = game.getTileAt(p);
 
-    @Override
-    public Player getPlayerInTurn() {
-        return null;
+        System.out.println(currTile.toString() + " at " + p.toString() + ".");
+
+        return currTile;
     }
 
     @Override
-    public Player getWinner() {
-        return null;
+    public Unit getUnitAt(Position p)
+    {
+        Unit currUnit = game.getUnitAt(p);
+
+        System.out.println(currUnit.toString() + " at " + p.toString() + ".");
+        return currUnit;
     }
 
     @Override
-    public int getAge() {
-        return 0;
+    public City getCityAt(Position p)
+    {
+        City currCity = game.getCityAt(p);
+
+        System.out.println(currCity.toString() + " at " + p.toString() + ".");
+        return currCity;
     }
 
     @Override
-    public boolean moveUnit(Position from, Position to) {
-        return false;
+    public Player getPlayerInTurn()
+    {
+        Player currPlayer = game.getPlayerInTurn();
+
+        System.out.println(currPlayer + " player is at turn now.");
+
+        return currPlayer;
     }
 
     @Override
-    public void endOfTurn() {
+    public Player getWinner()
+    {
+        Player winner = game.getWinner();
 
+        System.out.println(winner.toString() + " is the winner.");
+
+        return winner;
     }
 
     @Override
-    public void changeWorkForceFocusInCityAt(Position p, String balance) {
-
+    public int getAge()
+    {
+        return game.getAge();
     }
 
     @Override
-    public void changeProductionInCityAt(Position p, String unitType) {
+    public boolean moveUnit(Position from, Position to)
+    {
+        Player currPlayer = game.getPlayerInTurn();
+        Unit currUnit = game.getUnitAt(from);
+        boolean hasMoved = game.moveUnit(from, to);
 
+        if (hasMoved)
+        {
+            System.out.println(currPlayer.toString() + " player moves " + currUnit.toString() +
+                    " from " + from.toString() + " to " + to.toString());
+        } else
+        {
+            System.out.println(currPlayer.toString() + " player could not move " + currUnit.toString() +
+                    " from " + from.toString() + " to " + to.toString());
+        }
+
+        return hasMoved;
     }
 
     @Override
-    public void performUnitActionAt(Position p) {
+    public void endOfTurn()
+    {
+        Player currPlayer = game.getPlayerInTurn();
 
+        System.out.println(currPlayer.toString() + " player ends turn.");
+
+        game.endOfTurn();
     }
 
     @Override
-    public void addToWorld(Position p, Unit u) {
+    public void changeWorkForceFocusInCityAt(Position p, String balance)
+    {
+        Player currPlayer = game.getPlayerInTurn();
 
+        System.out.println(currPlayer.toString() + " player changes work force focus in city at " +
+                p.toString() + " to " + balance + ".");
+
+        game.changeWorkForceFocusInCityAt(p, balance);
     }
 
     @Override
-    public void addToWorld(Position p, City c) {
+    public void changeProductionInCityAt(Position p, String unitType)
+    {
+        Player currPlayer = game.getPlayerInTurn();
 
+        System.out.println(currPlayer.toString() + " player changes production in city at " +
+                p.toString() + " to " + unitType + ".");
+
+        game.changeProductionInCityAt(p, unitType);
     }
 
     @Override
-    public void removeFromWorld(Position p, Unit u) {
+    public void performUnitActionAt(Position p)
+    {
+        Player currPlayer = game.getPlayerInTurn();
+        Unit currUnit = game.getUnitAt(p);
 
+        System.out.println(currPlayer.toString() + " player performs unit action at " +
+                p.toString() + ".");
+
+        game.performUnitActionAt(p);
     }
 
     @Override
-    public void removeFromWorld(Position p, City c) {
+    public void addToWorld(Position p, Unit u)
+    {
+        System.out.println(u.toString() + " added at " + p.toString() + ".");
 
+        game.addToWorld(p, u);
+    }
+
+    @Override
+    public void addToWorld(Position p, City c)
+    {
+        System.out.println(c.toString() + " added at " + p.toString() + ".");
+
+        game.addToWorld(p, c);
+    }
+
+    @Override
+    public void removeFromWorld(Position p, Unit u)
+    {
+        System.out.println(u.toString() + " removed from " + p.toString() + ".");
+
+        game.removeFromWorld(p, u);
+    }
+
+    @Override
+    public void removeFromWorld(Position p, City c)
+    {
+        System.out.println(c.toString() + " removed from " + p.toString() + ".");
+
+        game.removeFromWorld(p, c);
     }
 }
